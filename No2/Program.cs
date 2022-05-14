2d6acb0afe8e6d007fc077e3a347fea534122ca9
Katherine_20 <mob1911494@gmail.com>
2022-05-14 10:44:30 +0300

    added No2

---
+++ No2/Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите прогу, которая выводит 3 цифру заданного числа или сообщает, что его нет
+Console.WriteLine("Введите любое натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num < 100) && (num > -100))
+{
+    Console.WriteLine($"{num} -> третьей цифры нет");
+}
+else 
+{
+    string str = num.ToString();
+    Console.WriteLine($"{num} -> {str[2]}");
+}
