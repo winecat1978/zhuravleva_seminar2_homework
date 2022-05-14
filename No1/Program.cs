e19a13966504dd43049f2615bda1287de0a16d31
Katherine_20 <mob1911494@gmail.com>
2022-05-14 10:31:21 +0300

    added No1

---
+++ No1/Program.cs
@@ -0,0 +1,12 @@
+﻿//напишите прогу, которая принимает на вход трехзначное число, а на выходе выдает вторую цифру этого числа
+Console.WriteLine("Введите любое трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num < 100) || (num > 1000)) 
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else 
+{
+    string str = num.ToString();
+    Console.WriteLine($"{num} -> {str[1]}");
+}
