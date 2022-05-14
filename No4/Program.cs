c2ec25b934c61a9f06ca8fd24b3a5835085f41e8
Katherine_20 <mob1911494@gmail.com>
2022-05-14 12:14:09 +0300

    added No4

---
+++ No4/Program.cs
@@ -0,0 +1,4 @@
+﻿// напишите прогу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+int num = new Random().Next (100,1000);
+string str = num.ToString();
+Console.WriteLine($"{num} -> {str[0]}{str[2]}");
