d05877d27b2aad35bd605daddec259d12802b321
Katherine_20 <mob1911494@gmail.com>
2022-05-14 12:00:46 +0300

    added No3

---
+++ No3/Program.cs
@@ -0,0 +1,10 @@
+﻿// написать прогу, которая на число от 1 до 7, а выдает является ли этот день выходным
+Console.WriteLine("Введите число от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num > 0) && (num < 6))
+{
+    Console.WriteLine("нет");
+}
+if (num == 6) Console.WriteLine("да");
+if (num == 7) Console.WriteLine("да");
+ 
